e776f1c2c5e1dc0a27a322916070a7fea62e1490
OscarBackman92 <jan.oscar.backman@gmail.com>
2025-02-26 08:36:47 +0100

    first commit in next.js

---
+++ budget-tracker-frontend/src/app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white text-3xl font-bold">
+      🚀 Tailwind CSS is Working in Next.js!
+    </div>
+  );
+}
