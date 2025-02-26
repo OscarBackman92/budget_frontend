ddd30ff11091a902e796138b036b8bd4da95895a
OscarBackman92 <jan.oscar.backman@gmail.com>
2025-02-26 16:03:40 +0100

    fix casesensativety

--- budget-tracker-frontend/src/app/dashboard/Page.jsx
+++ budget-tracker-frontend/src/app/dashboard/Page.jsx
@@ -2,7 +2,7 @@ export default function DashboardPage() {
   return (
     <div className="text-center py-10">
       <h1 className="text-3xl font-bold">📊 Dashboard</h1>
-      <p className="mt-4 text-gray-600">Manage your transactions and insights here.</p>
+      <p className="mt-4 text-gray-600">Manage your transactions here.</p>
     </div>
   );
 }
